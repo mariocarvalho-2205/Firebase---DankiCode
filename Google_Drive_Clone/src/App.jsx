a8ca821af1e762d196cbb65b02049fe3ce213a0b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-06-30 11:42:03 -0300

    login persistente

--- Google_Drive_Clone/src/App.jsx
+++ Google_Drive_Clone/src/App.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, provider } from "../src/firebase/firebaseConfig"
+import { auth, provider } from "../src/firebase/firebaseConfig";
 import { signInWithPopup } from "firebase/auth";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Home from "../src/pages/Home/Home";
-// import Login from '../src/pages/Login/Login'
+import Login from "../src/pages/Login/Login";
 import "./App.css";
 
 function App() {
   const [login, setLogin] = useState(false);
 
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("Logado", user.email);
+        setLogin(true);
+      } else {
+        setLogin(false);
+      }
+    });
+  }, []);
 
   const handleLogin = (e) => {
     e.preventDefault();
     console.log("login", auth);
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log("Resultado", result);
-      setLogin(true);
-    })
+    signInWithPopup(auth, provider).then((result) => {
+      if (result) {
+        console.log("Logado", result.user.email);
+        setLogin(true);
+      }
+    });
   };
 
   return (
@@ -26,13 +37,15 @@ function App() {
       <BrowserRouter>
         {login ? (
           <Routes>
-            <Route path="/home" element={login ? <Home /> : <Navigate to="/" />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={login ? <Home /> : <Navigate to="/" />} />
+            {/* <Route path="/" element={<Home />} /> */}
           </Routes>
         ) : (
           <div>
             <h1>Login</h1>
-            {/* <Route path="/" element={<Login/>} /> */}
+            {/* <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes> */}
             <a onClick={(e) => handleLogin(e)} href="#">
               Logar
             </a>
